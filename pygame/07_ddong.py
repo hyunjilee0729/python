17aeab2ad14e4236d947a19bc500c694af7d6e77
hyunjilee0729 <hyunjilee729@gmail.com>
2022-05-13 18:44:09 +0900

    0513 똥피하기 게임완성

---
+++ pygame/07_ddong.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import random
+
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height ))
+
+pygame.display.set_caption("똥피하기 - 코드플레이")
+
+clock = pygame.time.Clock()
+
+bg = pygame.image.load("pygame/source/bg.png")
+
+character = pygame.image.load("pygame/source/character.png")
+
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_xPos = (screen_width / 2) - (character_width / 2)
+character_yPos = screen_height - character_height
+
+enemy = pygame.image.load("pygame/source/enemy.png")
+enemy_size = enemy.get_rect().size
+enemy_width = enemy_size[0]
+enemy_height = enemy_size[1]
+enemy_xPos = random.randint(0, (screen_width / 2 - enemy_width / 2))
+enemy_yPos = 0 
+
+enemy_speed = 10
+
+game_font = pygame.font.Font(None, 40)
+
+total_time = 10
+
+start_ticks = pygame.time.get_ticks()
+
+to_x = 0
+character_speed = 10
+
+running = True
+while running:
+    dt = clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= 1
+            elif event.key == pygame.K_RIGHT:
+                to_x += 1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            
+    character_xPos += to_x 
+
+    if character_xPos < 0:
+        character_xPos = 0
+    elif character_xPos > screen_width - character_width:
+        character_xPos = screen_width - character_width
+
+    if character_yPos < 0:
+        character_yPos = 0
+    elif character_yPos > screen_height - character_height:
+        character_yPos = screen_height - character_height
+
+    character_rect = character.get_rect()
+    character_rect.left = character_xPos    
+    character_rect.top = character_yPos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_xPos
+    enemy_rect.top = enemy_yPos
+
+    enemy_yPos += enemy_speed
+    if enemy_yPos > screen_height:
+        enemy_yPos = 0
+        enemy_xPos = random.randint(0, (screen_width - enemy_width))
+        enemy_speed = random.randint(5, 15) 
+
+    if character_rect.colliderect(enemy_rect):
+        print("충돌! 충돌!")
+        running = False
+
+    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+    timer = game_font.render(str(int(total_time - elapsed_time)), True, (0, 0, 0))
+
+    # if total_time - elapsed_time <= 0:
+    #     print("타임아웃")
+    #     running = False
+
+            # print("2022년.4월.22일. 신서윤 사망. 사인, 증오의 매") 
+    screen.blit(bg, (0, 0))
+    screen.blit(character, (character_xPos, character_yPos))
+    screen.blit(enemy, (enemy_xPos, enemy_yPos))
+    # screen.blit(timer, (10, 10))
+    
+    pygame.display.update()
+    
+pygame.time.delay(2000)
+pygame.quit() 
