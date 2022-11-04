60af2d9c7c30608e979ed0c768376aff97f181ae
hyunjilee0729 <hyunjilee729@gami.com>
2022-11-04 18:57:50 +0900

    11월 4일 영어사전?

---
+++ python_basics/memorize.py
@@ -0,0 +1,9 @@
+eng = ["oxygen", "make a living", "greatly", "various", "wonder", "creature", "even", "show up", "appear", "surface"]
+kor = ["산소", "생계를 꾸리다", "크게", "다양한", "궁금하다", "생물", "훨씬", "나타나다", "나타나다", "표면"]
+
+
+
+
+
+
+
